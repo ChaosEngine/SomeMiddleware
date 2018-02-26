f52e52af7063aeddc02bbd17be9211fdaef5255b
ChaosEngine <andrzej.pauli@gmail.com>
2018-02-27 00:00:08 +0100

    initial commit

---
+++ WebApplication1/SomeMiddleware.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+	public class SomeMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly string _options;
+
+		public SomeMiddleware(RequestDelegate next, string options)
+		{
+			_next = next;
+			_options = options;
+		}
+
+		public async Task Invoke(HttpContext httpContext)
+		{
+			if (!RequestCheck(httpContext.Request))
+			{
+				await _next(httpContext);
+				return;
+			}
+
+			RespondWithIndexHtml(httpContext.Response);
+		}
+
+		private bool RequestCheck(HttpRequest request)
+		{
+			var indexPath = string.IsNullOrEmpty(_options) ? "/" : $"/{_options}/";
+			return (request.Method == "GET" && request.Path == indexPath);
+		}
+
+		private async void RespondWithIndexHtml(HttpResponse response)
+		{
+			response.StatusCode = 200;
+			response.ContentType = "text/html";
+			var some_html = $@"
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    <title>About - WebApplication1</title>
+	<link rel='stylesheet' href='/lib/bootstrap/dist/css/bootstrap.css' />
+	<link rel='stylesheet' href='/css/site.css' />
+</head>
+<body>
+
+blablabla {_options}
+ 
+</body>
+</html>";
+
+			await response.WriteAsync(some_html, Encoding.UTF8);
+
+			//var bytes = Encoding.UTF8.GetBytes(htmlBuilder.ToString());
+			//response.Body.Write(bytes, 0, bytes.Length);
+		}
+	}
+
+	public static class SomeMiddlewareExtensions
+	{
+		public static IApplicationBuilder UseSomeMiddleware(this IApplicationBuilder app, Func<string, string> setupAction)
+		{
+			string options = "";
+			options = setupAction?.Invoke(options);
+
+			app.UseMiddleware<SomeMiddleware>(options);
+
+			return app;
+		}
+	}
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace WebApplication1
+{
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
+
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				.Build();
+
+			host.Run();
+		}
+
+		public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddResponseCompression();
+			services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseResponseCompression();
+			if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+			app.UseSomeMiddleware(c =>
+			{
+				c += "some_middleware";
+				return c;
+			});
+		}
+    }
+}
